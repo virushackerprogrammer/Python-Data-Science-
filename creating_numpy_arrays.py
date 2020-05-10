bd05881c6bb7570af43a3ca80148656116c69c18
virushackerprogrammer <59498557+virushackerprogrammer@users.noreply.github.com>
2020-05-10 15:28:43 +0530

    Add files via upload

---
+++ creating_numpy_arrays.py
@@ -0,0 +1,14 @@
+my_list = [1,2,3,4,5,6,7,8,9,10]
+print(my_list)
+#to create a numpy array first import numpy as np and use np.array(<a>)
+#a refers to the variable or the object
+#this is using a var
+import numpy as np
+my_array = np.array(my_list)
+print(my_array)
+#this is using an object
+my_array1 = np.array([1,2,3,4,5,6,7,8,9,10])
+print(my_array1)
+
+
+
